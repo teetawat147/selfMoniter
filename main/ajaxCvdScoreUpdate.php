36c45f61b3c23cc4b77ebbbe2c392ecfe73c970a
Thanasak1412 <Thanasaksrisaeng@gmail.com>
2021-02-09 23:51:02 +0700

    Merge branch 'main' of https://github.com/teetawat147/selfMoniter into main

--- main/ajaxCvdScoreUpdate.php
+++ main/ajaxCvdScoreUpdate.php
@@ -1,17 +1,17 @@
 <?php
     include('../include/connection.php');
 
-    $sql="UPDATE cvdScore SET ( ". 
-    " cvdName,conclude,advice ".
-    " ,cvdMin,cvdMax ". 
-    " ) ".
-    " VALUES ( ".
-    ",'".$_POST['cvdName']."' ".
-    ",'".$_POST['conclude']."' ".
-    ",'".$_POST['advice']."' ".
-    ",'".$_POST['cvdMin']."' ".
-    ",'".$_POST['cvdMax']."' ".
-    " WHERE cvdScoreId ='".$_POST['cvdScoreId']."'";
+    // $sql="UPDATE cvdScore SET ( ". 
+    // " cvdName,conclude,advice ".
+    // " ,cvdMin,cvdMax ". 
+    // " ) ".
+    // " VALUES ( ".
+    // ",'".$_POST['cvdName']."' ".
+    // ",'".$_POST['conclude']."' ".
+    // ",'".$_POST['advice']."' ".
+    // ",'".$_POST['cvdMin']."' ".
+    // ",'".$_POST['cvdMax']."' ".
+    // " WHERE cvdScoreId ='".$_POST['cvdScoreId']."'";
 
     $sql = "UPDATE cvdScore SET ". 
     " cvdName='".$_POST['cvdName']."' ".
