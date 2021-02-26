651569def34dd5fbb71d463fd183396004e062aa
Thanasak <Thanasaksrisaeng@gmail.com>
2021-02-26 14:19:56 +0700

    update

--- main/departmentSaveUpdate.php
+++ main/departmentSaveUpdate.php
@@ -9,13 +9,6 @@
             " ,countPersonDept = '".$_POST['countPersonDept']."' ".
             " WHERE departmentId = '".$_POST['departmentId']."' ";
 
-    $sql="UPDATE department SET ". 
-            " officeId ='".$_POST['officeId']."' ".
-            " ,departmentName ='".$_POST['departmentName']."' ".
-            " ,personId ='".$_SESSION['personId']."' ".
-            " ,countPersonDept='".$_POST['countPersonDept']."' ".
-            " WHERE departmentId ='".$_POST['departmentId']."' ";
-
     $result = $conn -> prepare($sql)    ;
     $result -> execute();
     header("location: ../main/departmentGet.php");
